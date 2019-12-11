e5db0eca2f5915b4003ae1e95f567d6ded9d95f6
Adam Walker <adamw207@yahoo.com>
2019-12-11 23:42:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,3 +8,10 @@ end
 
 # code your input_to_index and move method here!
 #to_i
+def update_array_at_with(array, index, value)
+array [index = value]
+end
+
+update_array_at_with(board, 0, "X")
+# The element at index 0 of array 'board' is set to the value "X"
+board #=> ["X", " ", " "]
