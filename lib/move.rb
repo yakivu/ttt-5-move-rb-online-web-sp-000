ef610ec269903182a29a5b020571d0a8787b9aeb
Adam Walker <adamw207@yahoo.com>
2019-12-11 23:52:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ end
 # array[index = value]
 # end
 
- # def(board, 0, "X")
- # end
+ def(board, 0, "X")
+ end
   # The element at index 0 of array 'board' is set to the value "X"
  # board #=> ["X", " ", " "]
 
