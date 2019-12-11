c0bfdad2be66e1898938d1a4441c40a80af67725
Adam Walker <adamw207@yahoo.com>
2019-12-11 23:47:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -17,10 +17,7 @@ end
 # The element at index 0 of array 'board' is set to the value "X"
 board #=> ["X", " ", " "]
 
-def update_array_at_with(array, index, value)
-array [index = value]
-end
 
-update_array_at_with(board, 0, "X")
-# The element at index 0 of array 'board' is set to the value "X"
-board #=> ["X", " ", " "]
+
+board(board, 0, "X")
+board = ["X", " ", " "]
