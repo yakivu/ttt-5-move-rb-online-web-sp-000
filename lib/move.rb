e95bc3fbee23f1a45c13270f452c16e5bb8c514c
Adam Walker <adamw207@yahoo.com>
2019-12-12 00:00:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,7 @@ def update_array_at_with(array, index, value)
  end
 
  def input_to_index(input)
-   (0, "X")
+   (board, 0, "X")
  end
   # The element at index 0 of array 'board' is set to the value "X"
  # board #=> ["X", " ", " "]
